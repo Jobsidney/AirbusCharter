31e2b4e96c271d4e8255f5798745080b4285cbd2
Jobsidney <jobsidney67@gmail.com>
2026-01-26 17:25:52 +0300

    Add ContactHero component for contact page

---
+++ src/app/_components/contact-hero/ContactHero.tsx
@@ -0,0 +1,13 @@
+export default function ContactHero() {
+  return (
+    <section className="contactHeroSection section-padding bg-gradient">
+      <div className="contactHeroContent">
+        <h1 className="contactHeroTitle">CONTACT US</h1>
+        <p className="contactHeroDescription">
+          Luctus elit lobortis porta eleifend dictum venenatis viverra. Eu et dignissim condimentum aliquet nisl. At nulla quis volutpat risus volutpat nunc laoreet dignissim.
+        </p>
+      </div>
+    </section>
+  );
+}
+
