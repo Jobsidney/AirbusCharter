ea16765c8f6bbf98ae1b7753aec374c221c59261
Jobsidney <jobsidney67@gmail.com>
2026-01-26 17:25:49 +0300

    Add FleetHero component for fleet page

---
+++ src/app/_components/fleet-hero/FleetHero.tsx
@@ -0,0 +1,13 @@
+export default function FleetHero() {
+  return (
+    <section className="fleetHeroSection section-padding bg-gradient">
+      <div className="fleetHeroContent">
+        <h1 className="fleetHeroTitle">FLEET</h1>
+        <p className="fleetHeroDescription">
+          Luctus elit lobortis porta eleifend dictum venenatis viverra. Eu et dignissim condimentum aliquet nisl. At nulla quis volutpat risus volutpat nunc laoreet dignissim.
+        </p>
+      </div>
+    </section>
+  );
+}
+
